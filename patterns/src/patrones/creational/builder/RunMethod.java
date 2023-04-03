46a2ec60238119c0fe6ae768c2ba3ce1781af1ac
Christian Rodriguez <christianrodriguez@Christians-MacBook-Pro.local>
2023-04-02 23:54:35 -0500

    Ajustes menores

--- patterns/src/patrones/creational/builder/RunMethod.java
+++ patterns/src/patrones/creational/builder/RunMethod.java
@@ -3,10 +3,17 @@ package patrones.creational.builder;
 public class RunMethod {
 
     public static void runBuilder()  {
-        Card.CardBuilder card = new Card.CardBuilder().type("VISA").number("000 111 222 333").name("CHRISTIAN").expiration(2024).isCredit(false);
+        Card.CardBuilder card = new Card.CardBuilder()
+                .type("VISA")
+                .number("000 111 222 333")
+                .name("CHRISTIAN")
+                .expiration(2024)
+                .isCredit(false);
         System.out.println(card.build().toString());
 
-        Card.CardBuilder card2 = new Card.CardBuilder().type("VISA").number("000 111 222 333");
+        Card.CardBuilder card2 = new Card.CardBuilder()
+                .type("VISA")
+                .number("000 111 222 333");
         System.out.println(card2.build().toString());
     }
 
